dd748d1fb5e3f752774bb2b77bc0e8b9ae5f0900
nina <ninagbs@abv.bg>
2023-10-25 10:19:13 +0300

    added static data.js

---
+++ frontend/src/app/shared/models/Organization.ts
@@ -0,0 +1,12 @@
+export class Organization {
+    id!: string;
+    name!: string;
+    country!: string;
+    uic!: string;
+    vat?: string;
+    address?: string;
+    mol!: string;
+    teems?: string[];
+    employees?: string[];
+
+}
---
+++ frontend/src/data.ts
@@ -0,0 +1,38 @@
+import { Organization } from "./app/shared/models/Organization";
+
+export const sample_organizations: Organization[] = [
+    {
+        id: '1',
+        name: 'SoftUni',
+        country: 'Bulgaria',
+        uic: '123123123',
+        vat: 'BG123123123',
+        address: 'Sofia, Raiko Aleksiev 20A',
+        mol: 'Svetlin Nakov',
+        teems: ['Frontend', 'Backend', 'Sequrity'],
+        employees: ['Svetlin Nakov', 'Ivanka Ivanova', 'Petia Petrova']
+    },
+    {
+        id: '2',
+        name: 'Fakturico',
+        country: 'Bulgaria',
+        uic: '123123666',
+        vat: 'BG123123666',
+        address: 'Sofia, Ivan Geshev 20A',
+        mol: 'Martin Marinov',
+        teems: ['QA', 'Backend', 'Sequrity'],
+        employees: ['Martin Marinov', 'Ivancho Ivanov', 'Petar Petrov']
+    },
+    {
+        id: '3',
+        name: 'SomeCoolCompany',
+        country: 'Germany',
+        uic: '123123777',
+        vat: '',
+        address: 'Berlin, SomeCoolStreet 20AG',
+        mol: 'John Bouvi',
+        teems: ['Frontend', 'Backend', 'QA'],
+        employees: ['John Bouvi', 'Ivan Ivanonkovich', 'Peter Petrov']
+    },
+
+]
